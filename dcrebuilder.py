93bc8eab1a65ed095b2baf51ddca9ad79f592fce
Slugcat <jaanov2013@gmail.com>
2024-05-05 18:44:19 -0400

    yup thats a bot

---
+++ dcrebuilder.py
@@ -0,0 +1,13 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="dc!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Signed in as %s!" %bot.user)
+
+bot.run(open("token.txt", "r").read())
